386ed827faf4642d01f2bbb08fc3e2dab1909c0f
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-02-28 14:00:04 +0530

    Create java2.java

---
+++ Basics/java2.java
@@ -0,0 +1,13 @@
+package Basics;
+
+// **** VARIABLES IN JAVA ****//
+public class java2 {
+    public static void main(String[] args) {
+        // case sensitive
+        // can be letters , digits , dollar sign , or underscores
+        // must begin with a letter , dollar sign , or underscore
+        // cannot use java keywords as variable names
+        String myname = "Vanshu";
+        System.out.println(myname);
+    }
+}
