f2831feb058d0a82c7b02309f5576e16e9f2aa2f
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-02 20:08:51 +0530

    Create java3.java

---
+++ Basics/java3.java
@@ -0,0 +1,26 @@
+// **** ARITHMETIC OPERATION IN JAVA **** //
+public class java3 {
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 3;
+//        int c = a % b;
+//        int quotient = a / b;
+        int a = 10;
+        int b = 5;
+//        int result = a / b + 3 * 2 - 1 ;
+
+        int num = 9;
+        // int num2 = 8;
+
+        // int result = num1 + num2;
+        // num = num1 + 3;
+        //  num += 2;
+        //  num++;       // post-increment
+        //++num;          // pre-increment
+        //  num--;      //  decrement
+
+        int result = num++; // fetch the value and then increment
+        // int result = ++num; // first increment and then fetch the value
+        System.out.println(result);
+    }
+}
