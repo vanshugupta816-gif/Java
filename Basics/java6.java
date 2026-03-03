616fa82bbbbd48b607bc7cd65279c1a1c0d530f4
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-03 20:33:40 +0530

    Create java6.java

---
+++ Basics/java6.java
@@ -0,0 +1,35 @@
+// **** STRING METHOD **** //
+public class java7 {
+    static void main(String[] args) {
+//       String name = "vanshu";
+//        int length = name.length();
+//        char c = name.charAt(length - 1);
+//        String name2 = "vanshu";
+//        System.out.println(length);
+//        System.out.println(name.equals(name2));
+//        String str1 = "remote";
+//        String str2 = "car";
+//        System.out.println('r' + 0);
+//        System.out.println('c' + 0);
+//        int i = str2.compareTo(str2);
+//        System.out.println(i);
+
+//        String name = "Amar Panchal";
+//        System.out.println(name.startsWith("Am"));
+//        System.out.println(name.endsWith("al"));
+//        String str = "    ";
+//        System.out.println(str.isEmpty());
+//        System.out.println(str.isBlank());
+//        int i = name.lastIndexOf(str:"a");
+//        System.out.println(i);
+//        System.out.println(name.lastIndexOf(str:"pan"));
+//        System.out.println(name.indexOf(str:"a",5));
+
+        String name = "Amar Panchal";
+        System.out.println(name.substring(2, 8));
+        System.out.println(name.subSequence(2, 8));
+        // arp is a subsequence of amar panchal
+
+
+    }
+}
