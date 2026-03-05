74cdd50394466494eb93148f04aadeeab3245f4d
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-05 18:40:08 +0530

    Create java9.java

---
+++ Basics/java9.java
@@ -0,0 +1,24 @@
+// **** ARRAYS  **** // (collection of similar kind of data) //
+public class java10 {
+    public static void main(String[] args) {
+//        int x = 1;
+//       int y = 2;
+//        int z = 3;
+//        int[] array = {1, 2, 3};
+//       type[] variableName;
+//        int[] arr = new int[10];
+//        arr[3] = 43;
+//        arr[7] = 89;
+//        for(int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+//        int[] arr = new int[5];
+//        int[] arr1 = {1, 2, 3, 4};
+//        for(int i = 0; i < arr1.length; i++) {
+//            System.out.println(arr1[i]);
+//        }
+//        for(int i: arr1){
+//            System.out.println(i);
+//        }
+    }
+}
