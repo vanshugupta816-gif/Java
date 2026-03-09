917fdf3813d181784d01212ca32deb52cc08c0bc
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-09 20:05:01 +0530

    Create java17.java

---
+++ Basics/java17.java
@@ -0,0 +1,12 @@
+// **** WRAPPER CLASS **** //
+
+public class java19 {
+    static void main(String[] args) {
+        Integer b = 1;
+        Integer c = 2;
+        int x = 2;
+        int y = 2;
+        System.out.println(x == y);
+
+    }
+}
