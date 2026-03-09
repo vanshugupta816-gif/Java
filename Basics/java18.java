144bd63fcad43a6af6e65ca78c5d702fcdf328b5
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-09 20:05:34 +0530

    Create java18.java

---
+++ Basics/java18.java
@@ -0,0 +1,24 @@
+// **** MASTER MATH  **** //
+
+public class java20 {
+    static void main(String[] args) {
+        int a = 10;
+        int b = 11;
+        int max = Math.max(a , b);
+        int min = Math.min(a , b);
+        int c = -11;
+        int abs = Math.abs(c);
+        double d = 1.12;
+        double ceil = Math.ceil(d);
+        System.out.println(ceil);
+        System.out.println(Math.floor(d));
+        System.out.println(Math.round(d));
+        int e = 144;
+        System.out.println(Math.sqrt(e));
+        System.out.println(Math.pow(12,2));
+        System.out.println(Math.log(10));
+        System.out.println(Math.log10(10));
+        System.out.println(Math.PI + 1);
+
+    }
+}
