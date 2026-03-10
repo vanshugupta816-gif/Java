e5fe424069425e94ab20b7241e5a4a131ce70e68
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-10 20:36:40 +0530

    Create java2.java

---
+++ Inheritence/java2.java
@@ -0,0 +1,13 @@
+public class Cat extends Animal {
+//    public void makeSound() {
+//        System.out.println("Meow");
+//        String name;
+
+    @Override // this means we use @ that is called annotation
+    public void sayHello() {
+        System.out.println("Meow"); // override
+
+
+    }
+
+}
