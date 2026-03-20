ce1fa300c8e4808bacdffdaa1fb9a7b18051177d
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-20 20:44:53 +0530

    Create java4.java

---
+++ AccessModifier/java4.java
@@ -0,0 +1,20 @@
+public class Utitis {
+
+    // private constructor
+
+    public String name;
+    public int age;
+
+    public void sayHello() {
+        System.out.println("hello");
+    }
+
+    public static void sayBye() {
+        System.out.println("Bye...");
+    }
+}
+
+// public :- we can access it anywhere
+// private :- we can access only on the class level
+// default :- means no keyword
+// protected :- only those can access who extend that class
