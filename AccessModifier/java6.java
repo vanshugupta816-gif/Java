59eb48dd6fbb8386c845a62c104397e7b9910d0e
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-21 21:44:11 +0530

    Create java6.java

---
+++ AccessModifier/java6.java
@@ -0,0 +1,12 @@
+public class Test {
+    static void main(String[] args) {
+//        Utitis.sayBye();
+        School.getInstance();
+        School.getInstance();
+        School.getInstance();
+        School.getInstance();
+        School.getInstance();
+        School.getInstance();
+        School.getInstance();
+    }
+}
