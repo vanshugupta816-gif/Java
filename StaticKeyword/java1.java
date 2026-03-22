e633c65d57db5966f2501372f5c48d0b4e8ce9fd
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-22 17:02:35 +0530

    Create java1.java

---
+++ StaticKeyword/java1.java
@@ -0,0 +1,46 @@
+package Statickeyword;
+
+public class Student {
+
+    public static int count = 0;
+
+    static{       // static block
+        System.out.println("hello");
+    }
+
+    public Student(){
+        count++;
+    }
+
+    private int id;
+    private String name;
+    public int age;
+
+    private int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void getCount(){
+        System.out.println("total students: "+ count);
+    }
+}
