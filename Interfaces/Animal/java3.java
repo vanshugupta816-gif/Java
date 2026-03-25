50aa990c5267b3b06fa24983565bb5a943167a8d
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-25 20:59:41 +0530

    Create java3.java

---
+++ Interfaces/Animal/java3.java
@@ -0,0 +1,12 @@
+public class Dog implements Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dog is sleeping.");
+    }
+}
