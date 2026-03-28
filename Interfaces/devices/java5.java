4d1709ef5cf914587afeba5748a35b8a9cb6c4c1
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-03-28 12:56:32 +0530

    Create java5.java

---
+++ Interfaces/devices/java5.java
@@ -0,0 +1,11 @@
+public class test {
+    static void main(String[] args) {
+        Smartphone smartphone = new Smartphone();
+        smartphone.makeCall("8168976822");
+        smartphone.endCall();
+        smartphone.takePhoto();
+        smartphone.recordVideo();
+        smartphone.playMusic();
+        smartphone.stopMusic();
+    }
+}
