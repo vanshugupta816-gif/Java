69b04eb165a0637144748888e8497a72dbaea846
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-04-01 20:34:06 +0530

    Create java2.java

---
+++ Enums/java2.java
@@ -0,0 +1,17 @@
+public interface Dayclass {
+  
+    public static final String SUNDAY = "SUNDAY";
+
+    public static final String MONDAY = "MONDAY";
+
+    public static final String TUESDAY = "TUESDAY";
+
+    public static final String WEDNESDAY = "WEDNESDAY";
+
+    public static final String THURDAY = "THURSDAY";
+
+    public static final String FRIDAY = "FRIDAY";
+
+    public static final String SATURDAY = "SATURDAY";
+
+}
