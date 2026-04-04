d4eae90df510c1560671b1d209602cb553f3c2dc
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-04-04 19:53:11 +0530

    Create java1.java

---
+++ oops/java1.java
@@ -0,0 +1,22 @@
+//**** OOPS ****//
+public class Car {
+
+    String color;
+    String brand;
+    String model;
+    int year;
+    int speed;
+
+    public void accelerate(int increment) {
+        speed += increment;
+    }
+
+    public void brake(int decrement) {
+        speed -= decrement;
+        if (speed < 0) {
+            speed = 0;
+        }
+    }
+}
+
+
