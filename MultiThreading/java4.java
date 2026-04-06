b57cc634911bccf6063519ad0d2cd46cde5c21d7
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-04-06 21:25:31 +0530

    Create java4.java

---
+++ MultiThreading/java4.java
@@ -0,0 +1,9 @@
+
+// lambda expression connects
+//public class EngineeringStudent implements  Student {
+//
+//    @Override
+//    public String getBio(String name) {
+//        return name + " is engineering student !";
+//    }
+//}
