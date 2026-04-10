44c7bb91af5fc5477f363ce83c9531fb2bf5844f
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-04-10 21:23:10 +0530

    Create java12.java

---
+++ MultiThreading/java12.java
@@ -0,0 +1,15 @@
+public class MythreadC extends Thread{
+
+    private Counter counter;
+
+    public MythreadC(Counter counter){
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 1000; i++ ){
+            counter.increment();
+        }
+    }
+}
