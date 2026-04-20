8462308eb3c16626531e19bb8c9bd7a4eabade50
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-04-20 20:34:13 +0530

    Create java14.java

---
+++ Collection Framework/java14.java
@@ -0,0 +1,14 @@
+public class Hash {
+    static void main(String[] args) {
+        System.out.println(simpleHash("ABC"));
+        System.out.println(simpleHash("CBA "));
+    }
+
+    public static int simpleHash(String key) {
+        int sum = 0;
+        for(char c : key.toCharArray()){
+            sum += (int) c;
+        }
+        return sum % 10;
+    }
+}
