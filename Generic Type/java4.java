a802e369b932766b93088a6ae37b3851cd2f9436
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-05-07 21:06:18 +0530

    Create java4.java

---
+++ Generic Type/java4.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+// jaise jaise isme hum chizo ko add krte h vaise vaise size bdta rehta h
+public class Generic {
+    static void main(String[] args) {
+       // int[] arr = new int[5];
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("world");
+        String s = list.get(0);
+        String s1 = list.get(1);
+        //list.get(0);
+       // list.add(123);
+        //list.add(3.14);
+
+//        String str = (String) list.get(0);
+//        String integer = (String) list.get(1);
+    }
+}
