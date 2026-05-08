b9d2e52821812cb5f2007acae78be45290f55a2e
vanshugupta816-gif <vanshugupta816@gmail.com>
2026-05-08 19:52:44 +0530

    Create java5.java

---
+++ Generic Type/java5.java
@@ -0,0 +1,15 @@
+
+public class GenericContainer<T> implements Container<T> {
+
+    private T item;
+
+    @Override
+    public void add(T item) {
+        this.item = item;
+    }
+
+    @Override
+    public T get() {
+        return item;
+    }
+}
